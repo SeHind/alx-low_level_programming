4a255c086c0c6b4e30c711288083fc6f2e81b374
SeHind <hindsekkat12@gmail.com>
2023-03-20 15:58:17 -0700

    Task 1-3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,13 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
- * Description: 'Print the last digit of a variable n'
- * @n: a variable generated randomly
- * @lastDigit: is the last digit of the variable n
+ * Description: 'Prints the last digit of a variable n'
  * Return: Always 0
  */
+
 int main(void)
 {
 	int n;
@@ -27,7 +26,8 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n"
+				, n, lastDigit);
 	}
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Prints alphabet in lowercase and uppercase'
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	n = 65;
+	while (n <= 90)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
