731defd149f9d9cf460c9db81e3e5177dd373dbf
SeHind <hindsekkat12@gmail.com>
2023-03-23 17:00:00 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * isupper _ Checks if a charachter is uppercase or not 
+ * @n : INTEGER
+ * Return: 1 if n is uppercase, 0 otherwise
+ */
+
+int _isupper(int n)
+{
+	if ((n >= 'A') && (n <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for prototype
+ * void prototypes (void)
+ * int prototype (int)
+ */
+
+int _isupper(int c);
+
+#endif /* MAIN_H */
