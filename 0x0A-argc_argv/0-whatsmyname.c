7110c0cf2ae272b21c260e10722fa941580b2c6c
SeHind <hindsekkat12@gmail.com>
2023-04-10 17:32:13 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Count of arguments
+ * @argv: Arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for prototype
+ * void prototypes (void)
+ * int prototype (int)
+ */
+int _putchar(char c);
+
+#endif /* MAIN_H */
