5b042de6098a108c6e41d8ecac0037f6a5ca27f6
SeHind <hindsekkat12@gmail.com>
2023-04-10 18:59:39 -0700

    Task 0

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -7,20 +7,5 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int k;
-
-
-	while (*s)
-	{
-		for (k = 0; accept[k]; k++)
-		{
-		if (*s == accept[k])
-		return (s);
-		}
-	s++;
-	}
-
-
-return ('\0');
+	return ('\0');
 }
-
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-		
+
 /**
  * char *_strcpy - a function that copies the string pointed to by src
  * @dest: copy to
@@ -8,19 +8,5 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int l = 0;
-	int x = 0;
-
-
-	while (*(src + l) != '\0')
-	{
-		l++;
-	}
-	for ( ; x < l ; x++)
-	{
-		dest[x] = src[x];
-	}
-	dest[l] = '\0';
 	return (dest);
 }
-
